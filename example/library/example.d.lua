708e0f4d33405bb30dd570bfb6cf5bc7cda4eb59
carsakiller <carsakiller@gmail.com>
2022-11-18 02:26:40 -0500

    add: example definition project

---
+++ example/library/example.d.lua
@@ -0,0 +1,9 @@
+---@class ExampleObject
+---@field name string
+---@field active boolean
+
+---Says hello to a user
+---@param name string Name of user
+function sayHello(name)
+    print("Hello, " .. name .. "!")
+end
